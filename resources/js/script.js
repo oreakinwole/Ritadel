563c989eee2444a4a5c68b3a862cd6e2eef7f171
taydrey <taydrey@users.noreply.github.com>
2019-03-18 15:02:18 +0100

    Create script.js

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$('#regUser').click(function(){
+$("h4").text("Log In");
+$("#ip1").remove();
+$("#ip4").text("Log In");
+
+});
+	
+    $('select').formSelect();
+	
+});
