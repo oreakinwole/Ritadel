e5a0458ce42f48df8c6c9f654c0dedde6908ee32
taydrey <taydrey@users.noreply.github.com>
2019-03-20 20:09:15 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const meals = [
+    { id: 1, name:'rice'},
+    { id: 2, name:'plantain'},
+    { id: 3, name:'yam'},
+    { id: 4, name:'beans'},
+    { id: 5, name:'indomie'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ritadel catering services');
+});
+
+app.get('/api/meals', (req, res) => {
+    res.send(meals);
+});
+
+app.post('/api/meals', (req, res) => {
+    const meal = {
+        id: meals.length + 1,
+        name: req.body.name
+    };
+    meals.push(meal);
+    res.send(meal);
+});
+
+
+app.get('/api/meals/:id', (req, res) => {
+   const meal = meals.find(c => c.id === parseInt(req.params.id));
+   if (!meal) res.status(404).send('Course not found.');
+   res.send(meal);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
