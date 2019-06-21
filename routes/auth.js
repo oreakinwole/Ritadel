077d4e6e63395e70781d118531ba0e4230991459
taydrey <taydrey@users.noreply.github.com>
2019-06-21 04:38:01 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,35 @@
-const mongoose = require('mongoose');
+const Joi = require('joi');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 const {User} = require('../models/user');
 
-router.get('/', async (req, res) => {
-      const allusers = await User.find();
-      res.send(allusers );
-  });
-  
-router.post('/', async (req, res) => {
-   let user = await User.findOne({ email: req.body.email });
-   if (user) return res.status(400).send('User already registerd.');
+function validate(req) {
+    const schema = {
+        email:Joi.string().min(1).max(200).required().email(),
+        password:Joi.string().min(5).max(1024).required()
+    }; 
+    return Joi.validate(req, schema);
+}
 
-     const auser = new User({ 
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-      });
-      sauser = await auser.save();
-      res.send(sauser);  
+// All Api Calls for Auth Module
+  
+/* post */
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password.'); 
+
+    const token = user.generateAuthToken();
+    res.send(token);
 });
+
+
 
 module.exports = router;
