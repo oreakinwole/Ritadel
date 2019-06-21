c65f4a4ecf163207add3261ebc45e99ffd2b948f
taydrey <taydrey@users.noreply.github.com>
2019-06-21 04:38:13 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,35 @@
-const mongoose = require('mongoose');
+const authmd = require('../middleware/authmd');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
-const {User} = require('../models/user');
+const {User, validate} = require('../models/user');
 
-router.get('/', async (req, res) => {
-      const allusers = await User.find();
-      res.send(allusers );
-  });
-  
+
+
+// All Api Calls for Users Module
+
+ router.get('/me', authmd, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
+
+/* post */
 router.post('/', async (req, res) => {
-   let user = await User.findOne({ email: req.body.email });
-   if (user) return res.status(400).send('User already registerd.');
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-     const auser = new User({ 
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-      });
-      sauser = await auser.save();
-      res.send(sauser);  
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registerd.');
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password', 'isAdmin']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email', 'isAdmin']));  
 });
 
 module.exports = router;
