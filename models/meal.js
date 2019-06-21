f41f7eb4c52ca4bea064f58888fc8aed2231ca6a
taydrey <taydrey@users.noreply.github.com>
2019-06-21 04:34:52 +0100

    Update meal.js

--- models/meal.js
+++ models/meal.js
@@ -1,16 +1,33 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
+
+// Joi Validate Meal Schema
+function validateMeal(meal) {
+    const schema = {
+        name:Joi.string().min(1).max(100).required(),
+        price:Joi.number().min(1).max(200).required()
+    }; 
+    return Joi.validate(meal, schema);
+}
+
+// Mongoose Validate Meal Schema
 const mealSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        maxlength: 100
     },
-    
-    shortName: {
-        type: String
+    price: {
+        type: Number,
+        required: true,
+        minlength: 1,
+        maxlength: 200
     }
 });
 
 const Meal = mongoose.model('Meal', mealSchema);
 
 exports.Meal = Meal;
+exports.validate = validateMeal;
