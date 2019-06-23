61b39071bf61d0264e6e8e0f870958e99d12efa9
taydrey <taydrey@users.noreply.github.com>
2019-06-23 03:35:10 +0100

    Update menu.js

--- routes/menu.js
+++ routes/menu.js
@@ -1,3 +1,4 @@
+const admin = require('../middleware/admin');
 const authmd = require('../middleware/authmd');
 const express = require('express');
 const router = express.Router();
@@ -8,25 +9,33 @@ const { Meal} = require('../models/meal');
 // All Api Calls for Menu Module
 
 /* get */
-router.get('/', authmd, async (req, res) => {
-    const getMenu = await Menu.find();
-    res.send(getMenu);
+router.get('/', authmd, async (req, res, next) => {
+    try {
+        const getMenu = await Menu.find();
+        res.send(getMenu);
+    } catch (ex) {
+        next(ex);
+    }
+    
 });
 
 
 /* post */
-router.post('/:id', [authmd, admin], async (req, res) => {
+router.post('/:id', [authmd, admin], async (req, res, next) => {
+
+    try {
         const food = await Meal.findById(req.params.id);
         if (!food) return res.status(404).send('The Meal with the given ID was not found');
-    
-    
-        let menuobj = new Menu({ 
-            meal: food
+
+         let menuobj = new Menu({ 
+            mealItem: food
     });
-
-         menuobj = await menuobj.save();
-         res.send(menuobj);
+        menuobj = await menuobj.save();
+         res.send(menuobj); 
+    } catch (ex) {
+        next(ex);
+    }
  });
-
+ 
 
 module.exports = router;
