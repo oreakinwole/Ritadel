b4c174b4a73cafb889a4b8c343620340eecaaaa8
taydrey <taydrey@users.noreply.github.com>
2019-06-23 03:35:36 +0100

    Update ordermod.js

--- models/ordermod.js
+++ models/ordermod.js
@@ -2,7 +2,36 @@ const mongoose = require('mongoose');
 
 // Mongoose Validate Order Schema
 const orderSchema = new mongoose.Schema({
-    order: Object
+    menuId: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    price: {
+        type: Number,
+        required: true,
+        minlength: 1
+    },
+    modified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+   /*  byUser: {
+        type: String,
+        required: true,
+        minlength: 1
+    }, */
+    date: {
+        type: String,
+        required: true,
+        minlength: 1
+    }
 });
 
 const Order = mongoose.model('Order', orderSchema);
