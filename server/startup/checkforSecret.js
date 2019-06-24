87fd0198f9d6c974a7aa0eb62d42f03e9f332ca5
taydrey <taydrey@users.noreply.github.com>
2019-06-25 00:01:59 +0100

    Create checkforSecret.js

---
+++ server/startup/checkforSecret.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = function() {
+
+    // Check for the presence of the environment variable used for signing the auth token
+ if (!process.env.SECRET_KEY) {
+
+    throw new Error('RITADEL SECRET KEY IS MISSING');
+
+ }
+ else {
+     console.info('We found Ritadel Secret key');
+ }
+ 
+};
