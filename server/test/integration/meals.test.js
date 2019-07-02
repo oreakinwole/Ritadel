6e312296e299422006d5c94ab6de4075765c5504
taydrey <taydrey@users.noreply.github.com>
2019-07-02 16:29:37 +0100

    Update meals.test.js

--- server/test/integration/meals.test.js
+++ server/test/integration/meals.test.js
@@ -11,6 +11,8 @@ describe('/api/meals', () => {
        await Meal.remove({}); 
     });
 
+
+
     describe('GET/', () => {
         it('Should return all the meals in the base', async () => {
             await Meal.collection.insertMany([
@@ -22,6 +24,26 @@ describe('/api/meals', () => {
            expect(res.body.length).toBe(2);
            expect(res.body.some(g => g.name === 'meal1')).toBeTruthy();
            expect(res.body.some(g => g.name === 'meal2')).toBeTruthy();
+        });
+    });
+
+    describe('GET /:id', () => {
+        it('should return a meal if valid ID is passed', async () => {
+            const myMeal = new Meal({ name: 'fufu', price: 400 });
+            await myMeal.save();
+
+         const result = await request(server).get('/api/meals/' + myMeal._id);
+
+            expect(result.status).toBe(200);
+            expect(result.body).toHaveProperty('name', myMeal.name);
+
+        });
+    });
+
+    describe('POST', () => {
+        it('should return a 401 if client is not logged in', async () => {
+          const newEat = await request(server).post('/api/meals').send( { name:'Amala', price:700 });
+        expect(newEat.status).toBe(401);
         });
     });
 });
