40ae51cf168067fa55474a489430e85eb0c5f96f
taydrey <taydrey@users.noreply.github.com>
2019-07-02 16:27:41 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import MealReducer from './reducers/meal';
 import './index.css';
-import App from './App';
+import './vendors/ionicons-2.0.1/css/ionicons.min.css';
+import './vendors/fonts/font-awesome.min.css';
+import SetUpMeal from './components/admin/MealSetup/SetUpMeal';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    MealReducer
+);
+
+ReactDOM.render(
+    <Provider store ={store}>
+    <SetUpMeal />
+    </Provider>,
+    document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
