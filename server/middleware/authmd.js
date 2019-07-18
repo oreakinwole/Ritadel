e3311b969ab6506aed72c6fd1412f0d950f55a20
taydrey <taydrey@users.noreply.github.com>
2019-07-18 17:04:39 +0100

    Update authmd.js

--- server/middleware/authmd.js
+++ server/middleware/authmd.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 // Authorization Middleware to check for an authorized token generated when signing up or logging in
 module.exports = function (req, res, next) {
-    const token = req.header('x-auth-token');
+    const token = req.header('ritadelToken');
     if (!token) return res.status(401).send('Access denied. No token provided.');
 
     try {
