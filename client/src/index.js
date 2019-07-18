9b0b343227b9868e1022fa3918af2eddce5f9e74
taydrey <taydrey@users.noreply.github.com>
2019-07-18 17:04:42 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import MealReducer from './reducers/meal';
-// import AdminMenuReducer from './reducers/adminMenu';
-// import OrderReducer from './reducers/order';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers/index';
 import './index.css';
 import './vendors/ionicons-2.0.1/css/ionicons.min.css';
 import './vendors/fonts/font-awesome.min.css';
-// import Menu from './components/admin/menu/AdminMenu';
-// import SetUpMeal from './components/admin/MealSetup/SetUpMeal';
-// import UserOrder from './components/userorder/UserOrder';
 import App from './App';
+import ReduxThunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(
-    MealReducer
-);
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
     <Provider store ={store}>
        <App />
     </Provider>,
     document.getElementById('root')
-    );
+    )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
