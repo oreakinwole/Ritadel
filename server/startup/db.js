417f5712d1ec47e51cf49ffede0a76ec0875eb78
taydrey <taydrey@users.noreply.github.com>
2019-07-24 23:24:49 +0100

    Update db.js

--- server/startup/db.js
+++ server/startup/db.js
@@ -4,16 +4,13 @@ const config = require('config');
 module.exports = function() {
 
     // Connection to MongoDB using mongoose
-    // const db = config.get('db');
-    /* mongoose.set('useCreateIndex',true);
-    mongoose.connect(db, { useNewUrlParser: true }) 
-    .then(()=> console.info(`Connected to ${db}`));
+    
+        const db = config.get('db');
+        mongoose.set('useCreateIndex',true);
+        mongoose.connect(db, { useNewUrlParser: true }) 
+        .then(()=> console.info(`Connected to ${db}`));
 
-    console.info(`Connected to ${db}`); */
-
-    const theList = config.get('oreList');
-
-    console.info(theList);
-
+        console.info(`Connected to ${db}`);
     
 };
+
