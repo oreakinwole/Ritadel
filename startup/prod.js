c3fc30f54639b525f3aa27869d5bd7bfe54f1138
taydrey <taydrey@users.noreply.github.com>
2019-07-25 13:19:17 +0100

    setting production variables

--- startup/prod.js
+++ startup/prod.js
@@ -1,10 +1,8 @@
 const helmet = require('helmet');
 const compression = require('compression');
-const cors = require('cors');
 
 
 module.exports = function(app) {
     app.use(helmet());
     app.use(compression());
-    app.use(cors());
 };
