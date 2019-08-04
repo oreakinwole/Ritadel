6c7b710e7df45e1d40fc077d09bc7f24c32c23da
taydrey <taydrey@users.noreply.github.com>
2019-08-04 23:35:55 +0100

    Update BottomNav.js

--- client/src/components/userorder/BottomNav.js
+++ client/src/components/userorder/BottomNav.js
@@ -7,10 +7,10 @@ const Nav = props => (
     <section className="usernav">
 
            <Link to = "/usermenu"> <i className="ion-android-arrow-back icon-big" /> </Link>
-            <i className="ion-android-delete icon-big" />
-            <span className="icon-items"> Orders </span>
-            <i className="ion-android-refresh icon-big" />
-            <span className="loggeduser">{ props.username } </span>
+            <i className="ion-android-delete" />
+            <span> Orders </span>
+            <i className="ion-android-refresh" />
+            <span>{ props.username } </span>
             
     </section>
 );
